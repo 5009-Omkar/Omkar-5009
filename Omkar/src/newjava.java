0a75c81888124c83e9ed172d9c071d371b0c12bd
rutuja113 <rutujarachcha991.rr@gmail.com>
2020-04-09 17:22:04 +0530

    firstcommit

---
+++ Omkar/src/newjava.java
@@ -0,0 +1,22 @@
+
+public class newjava {
+	public static void main(String[] args) {
+		int num =654321;
+		int rev=0;
+		
+		while(num !=0)
+		{
+				rev = rev*10+num %10;
+			num=num/10;
+			
+		}
+		
+		System.out.println(rev );
+		
+
+	}
+
+
+
+
+}
